77b58fd17993d2ce6554d5cb50f412c877153aff
Ogreman <theogreman@gmail.com>
2014-02-27 17:11:41 +0000

    array

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from unipath import Path
 TEMPLATE_DIR = Path(__file__).ancestor(1).child("templates")
 
 app = FlaskAPI(__name__, template_folder=TEMPLATE_DIR)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///sqlite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
@@ -42,9 +42,9 @@ class Note(db.Model):
 
 
 @app.route("/index/", methods=['GET'])
-@set_renderers(HTMLRenderer)
+@set_renderers([HTMLRenderer])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route("/", methods=['GET', 'POST'])
